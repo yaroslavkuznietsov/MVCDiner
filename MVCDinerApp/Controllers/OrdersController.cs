f920255abb410f9875f6e06879d92baf57264faf
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-07-01 00:57:20 +0300

    Added OrderDisplayModel.cs for Display View in Orders

--- MVCDinerApp/Controllers/OrdersController.cs
+++ MVCDinerApp/Controllers/OrdersController.cs
@@ -53,7 +53,24 @@ namespace MVCDinerApp.Controllers
 
             int id = await _orderData.CreateOrder(order);
 
-            return RedirectToAction("Create");
+            return RedirectToAction("Display", new { id });
+        }
+
+
+        public async Task<IActionResult> Display(int id)
+        {
+            OrderDisplayModel displayOrder = new OrderDisplayModel();
+
+            displayOrder.Order = await _orderData.GetOrderById(id);
+
+            if (displayOrder.Order != null)
+            {
+                var food = await _foodData.GetFood();
+
+                displayOrder.ItemPurchased = food.Where(x => x.Id == displayOrder.Order.FoodId)
+                                                 .FirstOrDefault()?.Title;
+            }
+            return View(displayOrder);
         }
     }
 }
